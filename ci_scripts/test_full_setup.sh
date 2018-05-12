709f73b605431fc71e64ec2f9ca5a1cbe5718aa1
MFreidank <freidankm@yahoo.de>
2018-05-12 16:24:16 +0200

    fixed path in test_full_setup.sh

--- ci_scripts/test_full_setup.sh
+++ ci_scripts/test_full_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 OUTPUT_FILENAME="/tmp/setup_output.txt" 
-python3 ../setup.py install | sudo tee -a "$OUTPUT_FILENAME"
+python3 setup.py install | sudo tee -a "$OUTPUT_FILENAME"
 if grep "ERROR" "$OUTPUT_FILENAME"; then
     exit 1
 fi
