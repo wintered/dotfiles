90d471b535a020a23e4b2816e7f5930a0d096ade
MFreidank <freidankm@yahoo.de>
2018-05-12 16:47:54 +0200

    changed error detection

--- ci_scripts/test_full_setup.sh
+++ ci_scripts/test_full_setup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 OUTPUT_FILENAME="/tmp/setup_output.txt" 
 python3 setup.py install | sudo tee -a "$OUTPUT_FILENAME"
-if grep "ERROR" "$OUTPUT_FILENAME"; then
+NUM_ERRORS=`grep "ERROR" "$OUTPUT_FILENAME" | wc -l`
+if [[ "$NUM_ERRORS" -gt 0 ]]; then
+    echo "$NUM_ERRORS errors occurred.."
     exit 1
 fi
 exit 0
