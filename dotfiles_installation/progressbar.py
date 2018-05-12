0afc17c57432f6f2fb539d46114c5c4465257d32
MFreidank <freidankm@yahoo.de>
2018-05-12 16:30:26 +0200

    making people aware that they miss out on tqdm

--- dotfiles_installation/progressbar.py
+++ dotfiles_installation/progressbar.py
@@ -1,6 +1,12 @@
+import logging
 try:
     from tqdm import tqdm
 except ImportError:
+    logging.warn(
+        "Python package 'tqdm' is not installed.\n"
+        "If you would like to see a progressbar, cancel installation with "
+        "(Ctrl+C) and do 'pip3 install tqdm' first."
+    )
     TQDM_INSTALLED = False
 else:
     TQDM_INSTALLED = True
