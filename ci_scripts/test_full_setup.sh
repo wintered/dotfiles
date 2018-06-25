7b65a086f3b4eab95f5ec9a90f266475ff8226bb
MFreidank <freidankm@yahoo.de>
2018-06-25 11:13:28 +0200

    more verbose output in ci-tests

--- ci_scripts/test_full_setup.sh
+++ ci_scripts/test_full_setup.sh
@@ -2,8 +2,9 @@
 OUTPUT_FILENAME="/tmp/setup_output.txt" 
 python3 setup.py install | sudo tee -a "$OUTPUT_FILENAME"
 NUM_ERRORS=`grep "ERROR" "$OUTPUT_FILENAME" | wc -l`
+echo "$NUM_ERRORS errors occurred during installation.."
 if [[ "$NUM_ERRORS" -gt 0 ]]; then
-    echo "$NUM_ERRORS errors occurred.."
+    echo "There were errors during installation - STATUS: Build Failed"
     exit 1
 fi
 exit 0
