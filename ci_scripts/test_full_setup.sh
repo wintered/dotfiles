55a44676ad44eb31690c51cbbe92323714172006
MFreidank <freidankm@yahoo.de>
2018-11-13 12:01:10 +0100

    inventory

--- ci_scripts/test_full_setup.sh
+++ ci_scripts/test_full_setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 OUTPUT_FILENAME="/tmp/setup_output.txt" 
-ansible-playbook -vvvv -i setup/localhost_inventory.yml setup/install/install.yml
+ansible-playbook -vvvv -i setup/test_inventory.yml setup/install/install.yml
