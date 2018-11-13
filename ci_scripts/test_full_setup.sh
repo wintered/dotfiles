eca3d03a2ca4ba698f55d463edfed4663d23e5a6
MFreidank <freidankm@yahoo.de>
2018-11-13 11:30:59 +0100

    more verbose

--- ci_scripts/test_full_setup.sh
+++ ci_scripts/test_full_setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 OUTPUT_FILENAME="/tmp/setup_output.txt" 
-ansible-playbook -i setup/localhost_inventory.yml setup/install/install.yml
+ansible-playbook -vvvv -i setup/localhost_inventory.yml setup/install/install.yml
