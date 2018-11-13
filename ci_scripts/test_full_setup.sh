85ff63355573b07c8fa71750c6c05dcf1bea84cc
MFreidank <freidankm@yahoo.de>
2018-11-13 10:50:39 +0100

    update

--- ci_scripts/test_full_setup.sh
+++ ci_scripts/test_full_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 OUTPUT_FILENAME="/tmp/setup_output.txt" 
-ansible-playbook -K ../setup/install/install.yml | sudo tee -a "$OUTPUT_FILENAME"
+ansible-playbook -K setup/install/install.yml | sudo tee -a "$OUTPUT_FILENAME"
 NUM_ERRORS=`grep "ERROR" "$OUTPUT_FILENAME" | wc -l`
 echo "$NUM_ERRORS errors occurred during installation.."
 if [[ "$NUM_ERRORS" -gt 0 ]]; then
