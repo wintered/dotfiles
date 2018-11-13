73c32c1d126d63c2639a446277d4aa42893ca5e9
MFreidank <freidankm@yahoo.de>
2018-11-13 11:07:54 +0100

    fix: i3wm

--- ci_scripts/test_full_setup.sh
+++ ci_scripts/test_full_setup.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 OUTPUT_FILENAME="/tmp/setup_output.txt" 
-ansible-playbook setup/install/install.yml | sudo tee -a "$OUTPUT_FILENAME"
-NUM_ERRORS=`grep "ERROR" "$OUTPUT_FILENAME" | wc -l`
-echo "$NUM_ERRORS errors occurred during installation.."
-if [[ "$NUM_ERRORS" -gt 0 ]]; then
-    echo "There were errors during installation - STATUS: Build Failed"
-    exit 1
-fi
-exit 0
+ansible-playbook setup/install/install.yml
