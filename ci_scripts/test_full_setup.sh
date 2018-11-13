a749db2894e8b45d7b1bfebcce09e2f7283d1197
MFreidank <freidankm@yahoo.de>
2018-11-13 16:38:32 +0100

    test full setup with uninstall test

--- ci_scripts/test_full_setup.sh
+++ ci_scripts/test_full_setup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 OUTPUT_FILENAME="/tmp/setup_output.txt" 
 ansible-playbook -vvvv -i setup/test_inventory.yml setup/install/install.yml
+ansible-playbook -vvvv -i setup/test_inventory.yml setup/uninstall/uninstall.yml
